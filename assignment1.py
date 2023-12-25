24b6d96bc7111869f9ea3536e8068adcb309c864
yunsun <ys247071@gmail.com>
2023-12-25 14:10:35 +0800

    Added main function

--- assignment1.py
+++ assignment1.py
@@ -4,11 +4,41 @@ Date started: 19_Dec_2023
 GitHub URL: https://github.com/yunsunpark12/Assignment_1
 """
 
-
 def main():
-    """..."""
-    print("Song List 1.0 - by Your Name")
+    """
+    To call get_menu function to display the menu options, deciding which function to execute
+    and quitting from the program by showing the number of songs saved in the program.
+    """
+
+    file = open('songs.csv', 'r+')
+    data = file.read()
+
+    print("Song List 1.0 - by Yunsun Park")
+
+    songs = [r.split(",") for r in [r for r in data.split("\n")]]   # converting string into 2D array
+    songs.pop(-1)                                                   # removing the empty string caused by \n from the list
+    no_of_songs = len(songs)
+    print("{} songs loaded.".format(no_of_songs))
+    display_menu()
+
+    # Determining which function to execute depending on the choice the user made
+
+    choice = input('>>> ').upper()
+    while choice != 'Q':
+        if choice == 'D':
+            display_songs(songs)
+        elif choice == 'A':
+            new_song = add_new_song()           # getting new song
+            songs.append(new_song)
+        elif choice == 'C':
+            check_to_mark_as_completed(songs)
+        else:
+            print("Invalid menu choice")
+        display_menu()
+        choice = input('>>> ').upper()
 
 
-if __name__ == '__main__':
-    main()
+    print("{} songs saved to {}".format(len(songs), file.name))
+    print("Make some music!")
+    file.close()
+    quit()
